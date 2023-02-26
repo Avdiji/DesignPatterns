4dadda940a4f2b5f15f0539342624569d0180f66
avdiji <favdiji@googlemail.com>
2023-02-26 13:59:50 +0100

    - added cs singleton

---
+++ Singleton/CSharp/RandomizerSingleton.cs
@@ -0,0 +1,28 @@
+﻿namespace random.BuilderPattern.Singleton {
+
+    // Singleton Class to generate Random int, boolean and double
+    public class RandomizerSingleton {
+
+        // private static instance of this
+        private static RandomizerSingleton instance = new RandomizerSingleton();
+        private Random random;
+
+        //private Constructor
+        private RandomizerSingleton() { random = new Random(); }
+
+        // static getter for this
+        public static RandomizerSingleton Instance {
+            get {
+                if (instance == null) {
+                    instance = new RandomizerSingleton();
+                }
+                return instance;
+            }
+        }
+
+        // other methods...
+        public int NextInt() { return random.Next(); }
+        public double NextDouble() { return random.NextDouble(); }
+        public bool NextBool() { return random.Next(2) == 0; }
+    }
+}
